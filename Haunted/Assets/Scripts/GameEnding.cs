5271386e15e3db205d00c8d38d76eab5da100574
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-03-10 20:54:59 -0500

    finished adding ghosts and more gargoyles

--- Haunted/Assets/Scripts/GameEnding.cs
+++ Haunted/Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,8 +9,10 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit = false;
+    bool m_IsPlayerCaught = false;
     float m_Timer;
 
     // Start is called before the first frame update
@@ -23,17 +26,28 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 
@@ -43,5 +57,10 @@ public class GameEnding : MonoBehaviour
         {
             m_IsPlayerAtExit = true;
         }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
     }
 }
