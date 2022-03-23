21c08256b46dfc32cc0d4ec6da787b1373fa8d47
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-03-22 21:38:56 -0400

    fog encroaches

---
+++ Haunted/Assets/Scripts/Fog.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+
+    public float rotateSpeed;
+    public float fallSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * rotateSpeed * Time.deltaTime);
+        transform.position -= Vector3.down * fallSpeed * Time.deltaTime;
+    }
+}
