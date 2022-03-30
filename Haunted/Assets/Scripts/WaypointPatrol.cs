3ef0780af23dc3ddde7562f2b9722782c444dafd
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-03-30 13:19:44 -0400

    has moving "fog" plane

--- Haunted/Assets/Scripts/WaypointPatrol.cs
+++ Haunted/Assets/Scripts/WaypointPatrol.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
+    [SerializeField] Transform player;
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
 
@@ -12,21 +13,15 @@ public class WaypointPatrol : MonoBehaviour
 
     public bool InPlayerSight { get; set; }
 
-    void Start()
-    {
-        navMeshAgent.SetDestination(waypoints[0].position);
-    }
-
     void Update()
     {
-        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
-        {
-            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-        }
         if (InPlayerSight)
         {
             navMeshAgent.SetDestination(transform.position);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(player.position);
         }
     }
 }
