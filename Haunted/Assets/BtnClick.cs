7acd4c11797aea83d6824996f82213ca6e7e641f
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-04-05 16:55:34 -0400

    added Intro scene for instructions

---
+++ Haunted/Assets/BtnClick.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BtnClick : MonoBehaviour
+{
+    private void Start()
+    {
+        Button b = gameObject.GetComponent<Button>();
+        b.onClick.AddListener(BtnNewScene);
+    }
+    public void BtnNewScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
