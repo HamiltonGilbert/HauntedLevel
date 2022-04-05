86a162319c27d6e75fa00a6bef390ef894cb0419
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-04-05 17:24:36 -0400

    intro scene functional, beginning level blocking

--- Haunted/Assets/Scripts/Fog.cs
+++ Haunted/Assets/Scripts/Fog.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class Fog : MonoBehaviour
 {
+    public Transform player;
+    public GameEnding gameEnding;
     public float moveSpeed;
+    public int timeUntilDeath;
+
+    bool m_IsPlayerInRange;
+    float count = 0;
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        if (m_IsPlayerInRange)
+        {
+            count += Time.deltaTime;
+            if (count >= timeUntilDeath)
+                gameEnding.CaughtPlayer();
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
     }
 }
